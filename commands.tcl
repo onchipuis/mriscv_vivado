169aca59d9392fac59b73538d05f4dae20376b6b
Generic <you@example.com>
2017-04-07 11:42:51 -0500

    Fixed some references

---
+++ commands.tcl
@@ -0,0 +1,4 @@
+write_cfgmem -force -format MCS -size 16 -loaddata "up 0x0 add.dat" program.mcs
+write_cfgmem  -format mcs -size 16 -interface SPIx1 -loadbit "up 0x00000000 ./mriscv_vivado.runs/impl_1/impl_axi.bit " -loaddata "up 0x00002000 ./ddr2.dat " -file "./program.mcs"
+riscv32-unknown-elf-gcc -c add.S
+riscv32-unknown-elf-objcopy -O binary add.o add.dat
